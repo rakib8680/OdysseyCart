97e30eb301fea89164050bd496010e9a3f4473d2
rakib8680 <rakib.cst.1st@gmail.com>
2026-04-30 18:39:31 +0600

    fix: updated auth pages padding for responsive devices

--- components/form/AuthFormWrapper.tsx
+++ components/form/AuthFormWrapper.tsx
@@ -12,7 +12,7 @@ export function AuthFormWrapper({
   children,
 }: AuthFormWrapperProps) {
   return (
-    <div className="flex-1 flex flex-col justify-center py-24 sm:px-6 lg:px-8 bg-slate-50">
+    <div className="flex-1 flex flex-col justify-center py-20 md:pt-0  pb-24 px-6 bg-slate-50 md:min-h-screen">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-slate-900">
           {title}
@@ -21,7 +21,7 @@ export function AuthFormWrapper({
       </div>
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-2xl sm:px-10 border border-slate-200">
+        <div className="bg-white py-12 px-6 shadow rounded-2xl sm:px-10 border border-slate-200">
           {children}
         </div>
       </div>
